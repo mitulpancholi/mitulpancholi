e5602a6547162993804260472d6588b05fe1392d
Mitul Pancholi <mitul.pancholi@live.com>
2021-06-10 10:49:47 +0530

    menu changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -24,6 +24,47 @@ const Navbar = () => {
           </div>
         </div>
       </div>
+      <div
+        className={
+          hamburger ? "menu__wrapper active" : "menu__wrapper"
+        }
+      >
+        <div className="menu__listing">
+          <div className="list__wrapper">
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Work</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        </div>
+        <div className="showcase__wrapper">
+          <span>L</span>
+          <span>E</span>
+          <span>T</span>
+          <span>'</span>
+          <span>S</span>
+          <span> </span>
+          <span>M</span>
+          <span>A</span>
+          <span>K</span>
+          <span>E</span>
+          <span></span>
+          <span>A</span>
+          <span> </span>
+          <span>D</span>
+          <span>I</span>
+          <span>F</span>
+          <span>F</span>
+          <span>E</span>
+          <span>R</span>
+          <span>E</span>
+          <span>N</span>
+          <span>C</span>
+          <span>E</span>
+        </div>
+      </div>
     </div>
   );
 };
