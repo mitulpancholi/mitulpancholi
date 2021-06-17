64e6c73829367178dd9bf1ee1e1bc167cb4e7488
Mitul Pancholi <mitul.pancholi@live.com>
2021-06-17 18:43:56 +0530

    animation  to name

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import gsap from "gsap";
 import "./styles/App.scss";
 
 // Component Import
 import Navbar from "./components/navbar";
 
 // Page Import
-import Home from "./pages/home"
+import Home from "./pages/home";
 
 function App() {
+  // wait for page to load then show body
+
+  gsap.from("body", {
+    duration: 1,
+    css: {
+      visibility: "hidden",
+      opacity: 0,
+    },
+  });
+
   return (
-    <div className='App'>
+    <div className="App">
       <Navbar />
-      <Home/>
+      <Home />
     </div>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import gsap from "gsap";
+
+const tl = gsap.timeline();
 
 const Home = () => {
+  useEffect(() => {
+    tl.from(".title__text h1", {
+      duration: 1.0,
+      // opacity: 0,
+      y: 150,
+      ease: "Power4.out",
+      skewY: 7,
+      delay: 1,
+      stagger: {
+        amount: 0.3,
+      },
+    });
+  }, []);
+
   return (
     <div className="home__wrapper">
       <div className="container">
         <div className="row">
           <div className="title__text">
-            <h1>Mitul Pancholi</h1>
+            <div style={{ overflow: "hidden" }}>
+              <h1 className="full__name">Mitul Pancholi</h1>
+            </div>
             {/* <h1>Self Taught Frontend Web Developer </h1> */}
-            <h2>Front-End Developer</h2>
+            <div style={{ overflow: "hidden" }}>
+              <h1 className="name__desc">Front-End Developer</h1>
+            </div>
           </div>
         </div>
       </div>
