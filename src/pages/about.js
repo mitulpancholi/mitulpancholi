d7c45a0eb703720a0f7ff427b32725d4c694c341
Mitul Pancholi <mitul.pancholi@live.com>
2021-07-09 23:43:43 +0530

    about us gsap half done

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
+  useEffect(() => {
+    gsap.from(".about__title", {
+      scrollTrigger: {
+        trigger: ".about__section",
+        start: "start center+=200",
+        end: "end center+=50",
+        scrub: 1,
+        // pin: true,
+        // markers: true,
+      },
+      xPercent: "-10",
+    });
+  });
+
   return (
     <section className="about__section">
       <div className="about__wrapper">
-        <div>About Me</div>
+        <div className="about__title">About Me</div>
         <h2>
           Front-end developer who cares deeply about user experience. Design to
           Engage & Impact.
